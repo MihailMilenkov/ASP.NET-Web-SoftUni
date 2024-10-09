f91f0478763db1e219d53e06b4a79c2e9e0bf7b1
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-10-09 16:22:59 +0300

    Numbers from 1 to 20

--- 01FirstStepsInProgramming.cs
+++ 01FirstStepsInProgramming.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //
-Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
 
+// Expressions
+//Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+
+// Numbers from 1 to 20
+
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine(i+1);
+}
