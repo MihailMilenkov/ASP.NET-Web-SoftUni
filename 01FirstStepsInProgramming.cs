d590edf20c122d813e7e1f52cdb7a2742d656761
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-10-09 16:28:17 +0300

    Rectangle area

--- 01FirstStepsInProgramming.cs
+++ 01FirstStepsInProgramming.cs
@@ -1,12 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //
 
-// Expressions
+// 1. Expressions
 //Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
 
-// Numbers from 1 to 20
+// 2. Numbers from 1 to 20
+//for (int i = 0; i < 20; i++)
+//{
+//    Console.WriteLine(i+1);
+//}
 
-for (int i = 0; i < 20; i++)
-{
-    Console.WriteLine(i+1);
-}
+// 3. Rectangle area
+decimal a = decimal.Parse(Console.ReadLine());
+var b = decimal.Parse(Console.ReadLine());
+Console.WriteLine((2 * a) + (2 * b));
