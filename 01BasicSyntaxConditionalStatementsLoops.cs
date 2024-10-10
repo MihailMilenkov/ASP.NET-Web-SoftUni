fa796875ff865c77c190f81616e8334b8dd9544d
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-10-10 10:13:21 +0300

    Division

--- 01BasicSyntaxConditionalStatementsLoops.cs
+++ 01BasicSyntaxConditionalStatementsLoops.cs
@@ -2,25 +2,53 @@
 //
 
 // 1. Age group
-int age = int.Parse(Console.ReadLine());
+//int age = int.Parse(Console.ReadLine());
 
-if (age >= 0 && age <= 2)
+//if (age >= 0 && age <= 2)
+//{
+//    Console.WriteLine("baby");
+//}
+//else if (age <= 13)
+//{
+//    Console.WriteLine("child");
+//}
+//else if (age <= 19)
+//{
+//    Console.WriteLine("teenager");
+//}
+//else if (age <= 65)
+//{
+//    Console.WriteLine("adult");
+//}
+//else if (age >= 66)
+//{
+//    Console.WriteLine("elder");
+//}
+
+// 2. Division
+int dividend = int.Parse(Console.ReadLine());
+
+if (dividend % 10 == 0)
 {
-    Console.WriteLine("baby");
+    Console.WriteLine("The number is divisible by 10");
 }
-else if (age <= 13)
+else if (dividend % 7 == 0)
 {
-    Console.WriteLine("child");
+    Console.WriteLine("The number is divisible by 7");
 }
-else if (age <= 19)
+else if (dividend % 6 == 0)
 {
-    Console.WriteLine("teenager");
+    Console.WriteLine("The number is divisible by 6");
 }
-else if (age <= 65)
+else if (dividend % 3 == 0)
 {
-    Console.WriteLine("adult");
+    Console.WriteLine("The number is divisible by 3");
 }
-else if (age >= 66)
+else if (dividend % 2 == 0)
 {
-    Console.WriteLine("elder");
+    Console.WriteLine("The number is divisible by 2");
+}
+else
+{
+    Console.WriteLine("Not divisible");
 }
