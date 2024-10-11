e9ae6f6563e8c09c01a2049d16c91a0534a0a29e
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-10-11 16:04:18 +0300

    Vacation

--- 01BasicSyntaxConditionalStatementsLoops.cs
+++ 01BasicSyntaxConditionalStatementsLoops.cs
@@ -26,29 +26,105 @@
 //}
 
 // 2. Division
-int dividend = int.Parse(Console.ReadLine());
+//int dividend = int.Parse(Console.ReadLine());
 
-if (dividend % 10 == 0)
+//if (dividend % 10 == 0)
+//{
+//    Console.WriteLine("The number is divisible by 10");
+//}
+//else if (dividend % 7 == 0)
+//{
+//    Console.WriteLine("The number is divisible by 7");
+//}
+//else if (dividend % 6 == 0)
+//{
+//    Console.WriteLine("The number is divisible by 6");
+//}
+//else if (dividend % 3 == 0)
+//{
+//    Console.WriteLine("The number is divisible by 3");
+//}
+//else if (dividend % 2 == 0)
+//{
+//    Console.WriteLine("The number is divisible by 2");
+//}
+//else
+//{
+//    Console.WriteLine("Not divisible");
+//}
+
+// 3
+Vacation();
+
+void Vacation()
 {
-    Console.WriteLine("The number is divisible by 10");
-}
-else if (dividend % 7 == 0)
-{
-    Console.WriteLine("The number is divisible by 7");
-}
-else if (dividend % 6 == 0)
-{
-    Console.WriteLine("The number is divisible by 6");
-}
-else if (dividend % 3 == 0)
-{
-    Console.WriteLine("The number is divisible by 3");
-}
-else if (dividend % 2 == 0)
-{
-    Console.WriteLine("The number is divisible by 2");
-}
-else
-{
-    Console.WriteLine("Not divisible");
+    Console.WriteLine("Number of people");
+    int? peopleCount = int.Parse(Console.ReadLine());
+    Console.WriteLine("Type: Students, Business, Regular");
+    string? type = Console.ReadLine();
+    Console.WriteLine("Day: Friday, Saturday, Sunday");
+    string? day = Console.ReadLine();
+    decimal pricePerPerson = 0;
+
+    switch (type)
+    {
+        case "Students":
+            switch (day)
+            {
+                case "Friday":
+                    pricePerPerson = 8.45M;
+                    break;
+                case "Saturday":
+                    pricePerPerson = 9.80M;
+                    break;
+                case "Sunday":
+                    pricePerPerson = 10.46M;
+                    break;
+            }
+            break;
+        case "Business":
+            switch (day)
+            {
+                case "Friday":
+                    pricePerPerson = 10.9M;
+                    break;
+                case "Saturday":
+                    pricePerPerson = 15.60M;
+                    break;
+                case "Sunday":
+                    pricePerPerson = 16;
+                    break;
+            }
+            break;
+        case "Regular":
+            switch (day)
+            {
+                case "Friday":
+                    pricePerPerson = 15;
+                    break;
+                case "Saturday":
+                    pricePerPerson = 20;
+                    break;
+                case "Sunday":
+                    pricePerPerson = 22.50M;
+                    break;
+            }
+            break;
+    }
+
+    decimal? totalPrice = pricePerPerson * peopleCount;
+    if (type == "Students" && peopleCount >= 30)
+    {
+        totalPrice *= .85M;
+    }
+    if (type == "Business" && peopleCount >= 100)
+    {
+        totalPrice -= pricePerPerson * 10;
+    }
+
+    if (type == "Regular" && peopleCount >= 10 && peopleCount <= 20)
+    {
+        totalPrice *= .95M;
+    }
+    Console.WriteLine($"Total price: {totalPrice:f2}");
 }
