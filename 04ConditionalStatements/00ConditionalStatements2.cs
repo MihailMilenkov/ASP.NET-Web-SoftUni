fc069c5724cac53e7222f621d11bcc13a3f897a2
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-10-12 18:55:40 +0300

    Conditional statements

---
+++ 04ConditionalStatements/00ConditionalStatements2.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpProgrammingBasics._04ConditionalStatements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class _00ConditionalStatements2
+    {
+    }
+}
---
+++ 04ConditionalStatements/00ConditionalStatements3.cs
@@ -0,0 +1,48 @@
+﻿namespace CSharpProgrammingBasics._04ConditionalStatements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class _00ConditionalStatements3
+    {
+        internal static void Start()
+        {
+            //==
+            //!=
+            //<
+            //<=
+            //>
+            //>=
+            //a == 97
+            //b == 98
+            //ture/false - bool
+
+
+            //Console.WriteLine("H" != "H");
+            //Console.WriteLine("H" > "H"); nemoje zashtoto sa stringove
+
+
+            //string => char - > int
+            //string firstName = Console.ReadLine();
+            //string lastName = Console.ReadLine();
+            //Console.Writeline()
+
+
+            //string password = Console.ReadLine();
+            //string ourPassword = "123";
+            //if(password == ourPassword)
+            //    {
+            //        Console.WriteLine("Logged In");
+            //    }
+
+            int a = 7;
+            int b = 2;
+            double result = a / b;
+
+            Console.WriteLine(result);
+        }
+    }
+}
