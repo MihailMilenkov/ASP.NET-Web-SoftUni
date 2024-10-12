2782ef6eb2881d0b15a42b350afb17c5ab6b57e8
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-10-12 18:56:24 +0300

    Conditional statements

--- 04ConditionalStatements/00ConditionalStatements1.cs
+++ 04ConditionalStatements/00ConditionalStatements1.cs
@@ -8,5 +8,22 @@
 
     internal class _00ConditionalStatements1
     {
+        internal static void Start()
+        {
+            static void Main(string[] args)
+            {
+                string password = Console.ReadLine();
+                string ourPassword = "123";
+
+                if (password == ourPassword)
+                {
+                    Console.WriteLine("Bravo");
+                }
+                else
+                {
+                    Console.WriteLine("Please try again");
+                }
+            }
+        }
     }
 }
--- 04ConditionalStatements/00ConditionalStatements3.cs
+++ 04ConditionalStatements/00ConditionalStatements3.cs
@@ -8,41 +8,6 @@
 
     internal class _00ConditionalStatements3
     {
-        internal static void Start()
-        {
-            //==
-            //!=
-            //<
-            //<=
-            //>
-            //>=
-            //a == 97
-            //b == 98
-            //ture/false - bool
-
-
-            //Console.WriteLine("H" != "H");
-            //Console.WriteLine("H" > "H"); nemoje zashtoto sa stringove
-
-
-            //string => char - > int
-            //string firstName = Console.ReadLine();
-            //string lastName = Console.ReadLine();
-            //Console.Writeline()
-
-
-            //string password = Console.ReadLine();
-            //string ourPassword = "123";
-            //if(password == ourPassword)
-            //    {
-            //        Console.WriteLine("Logged In");
-            //    }
-
-            int a = 7;
-            int b = 2;
-            double result = a / b;
-
-            Console.WriteLine(result);
-        }
+        
     }
 }
