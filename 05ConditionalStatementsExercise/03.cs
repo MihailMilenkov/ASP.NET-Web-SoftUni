8196bd974eaaaa8e1683c47e0e142972e118130a
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-10-12 20:28:28 +0300

    Conditional statements

---
+++ 05ConditionalStatementsExercise/03.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpProgrammingBasics._05ConditionalStatementsExercise
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class _03
+    {
+        internal static void Start()
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalTime = firstTime + secondTime + thirdTime;
+
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            Console.WriteLine($"{minutes}:{seconds:d2}");
+
+            //if (seconds < 10)
+            //{
+            //    Console.WriteLine($"{minutes}:0{seconds}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{minutes}:{seconds}");
+            //}
+        }
+    }
+}
