47b2445818a192ed3428d59913f8a8c6328c0b73
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-10-12 19:01:31 +0300

    Excellent result

---
+++ 04ConditionalStatements/01ExcellentResult.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpProgrammingBasics._04ConditionalStatements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class _01ExcellentResult
+    {
+        internal static void Start()
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
