cf245e5f5678a1ec1e8bf81d4417ba2bd44ade86
Mihail Milenkov <mihailmilenkov681@gmail.com>
2024-12-30 17:50:48 +0200

    Integer operations

---
+++ 02CSharpFundamentals/01DataTypesAndVariables/01IntegerOperations.cs
@@ -0,0 +1,21 @@
+﻿namespace _02CSharpFundamentals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class _01IntegerOperations
+    {
+        internal static void Start()
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+            int three = int.Parse(Console.ReadLine());
+            int four = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((one + two) / three * four);
+        }
+    }
+}
